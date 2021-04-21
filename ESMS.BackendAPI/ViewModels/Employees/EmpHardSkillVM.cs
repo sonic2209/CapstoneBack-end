a9d1229c7f11e096fb99c9e200f8eb8ae155e56b
sonic2209 <51818304+sonic2209@users.noreply.github.com>
2021-04-21 09:59:03 +0700

    edit loadEmpInfo feature

---
+++ ESMS.BackendAPI/ViewModels/Employees/EmpHardSkillVM.cs
@@ -0,0 +1,16 @@
+﻿using ESMS.BackendAPI.ViewModels.Certification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESMS.BackendAPI.ViewModels.Employees
+{
+    public class EmpHardSkillVM
+    {
+        public int SkillID { get; set; }
+        public int? SkillLevel { get; set; }
+        public List<EmpCertificationDetail> EmpCertifications { get; set; }
+        public List<ListCertificationViewModel> CertiList { get; set; }
+    }
+}
