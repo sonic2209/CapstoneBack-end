9a72fc4d4d2a361f705f812bac8f7bdd92a1d4e9
TomLesky <dinhbinh599@gmail.com>
2021-05-03 11:17:12 +0700

    add EmpCreate and Update validator

--- ESMS.BackendAPI/ViewModels/Employees/EmpCreateRequestValidator.cs
+++ ESMS.BackendAPI/ViewModels/Employees/EmpCreateRequestValidator.cs
@@ -10,7 +10,7 @@ namespace ESMS.BackendAPI.ViewModels.Employees
         public EmpCreateRequestValidator()
         {
             RuleFor(x => x.Name).NotEmpty().WithMessage("Name cannot be empty")
-                .MaximumLength(50).WithMessage("Name cannot exceed 50 characters");
+                .MaximumLength(50).WithMessage("Name cannot exceed 50 characters").Matches(@"^(?:[^\W\d_]| )+$");//Matches(@"/^[a-zA-Z\s]*$/");
 
             RuleFor(x => x.Email).NotEmpty().WithMessage("Email cannot be empty")
                 .Matches(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$").WithMessage("Email format is not correct");
@@ -21,7 +21,7 @@ namespace ESMS.BackendAPI.ViewModels.Employees
             RuleFor(x => x.Password).NotEmpty().WithMessage("Password cannot be empty")
                 .MinimumLength(6).WithMessage("Password needs to have at least 6 characters");
             RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Confirm Password cannot be empty");
-            RuleFor(x => x.IdentityNumber).NotEmpty().WithMessage("Identity Number cannot be empty");
+            RuleFor(x => x.IdentityNumber).NotEmpty().WithMessage("Identity Number cannot be empty").Matches("^[0-9]+$");
             RuleFor(x => x.Address).NotEmpty().WithMessage("Address cannot be empty");
 
             RuleFor(x => x).Custom((request, context) =>
---
+++ ESMS.BackendAPI/ViewModels/Employees/EmpUpdateRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESMS.BackendAPI.ViewModels.Employees
+{
+    public class EmpUpdateRequestValidator : AbstractValidator<EmpUpdateRequest>
+    {
+        public EmpUpdateRequestValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name cannot be empty")
+                .MaximumLength(50).WithMessage("Name cannot exceed 50 characters").Matches(@"^(?:[^\W\d_]| )+$");
+
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email cannot be empty")
+                .Matches(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$").WithMessage("Email format is not correct");           
+            RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone number cannot be empty").Matches(@"(0[1-9])+([0-9]{8})\b").WithMessage("Phone number format is not correct");
+            RuleFor(x => x.IdentityNumber).NotEmpty().WithMessage("Identity Number cannot be empty").Matches("^[0-9]+$");
+            RuleFor(x => x.Address).NotEmpty().WithMessage("Address cannot be empty");
+        }
+    }
+}
