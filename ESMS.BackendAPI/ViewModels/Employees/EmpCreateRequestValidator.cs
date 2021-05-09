931ee64658e7b6ef6a1df73def95d3ab7fadd66f
TomLesky <dinhbinh599@gmail.com>
2021-05-09 22:24:49 +0700

    Update Emp Validator

--- ESMS.BackendAPI/ViewModels/Employees/EmpCreateRequestValidator.cs
+++ ESMS.BackendAPI/ViewModels/Employees/EmpCreateRequestValidator.cs
@@ -9,10 +9,10 @@ namespace ESMS.BackendAPI.ViewModels.Employees
     {
         public EmpCreateRequestValidator()
         {
-            RuleFor(x => x.Name).NotEmpty().WithMessage("Name cannot be empty")
-                .MaximumLength(50).WithMessage("Name can not exceed 50 characters").Matches(@"^(?:[^\W\d_]| )+$");//Matches(@"/^[a-zA-Z\s]*$/");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name can not be empty")
+                .MaximumLength(50).WithMessage("Name can not exceed 50 characters").Matches(@"^(?:[^\W\d_]| )+$").WithMessage("Name can not contain digits or special characters");//Matches(@"/^[a-zA-Z\s]*$/");
 
-            RuleFor(x => x.Email).NotEmpty().WithMessage("Email cannot be empty").MaximumLength(50).WithMessage("Email can not exceed 50 characters")
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email can not be empty").MaximumLength(50).WithMessage("Email can not exceed 50 characters")
                 .Matches(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$").WithMessage("Email is not in the correct format");
             int i = 0;
             //RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone number cannot be empty").Length(10).Must(x=> int.TryParse(x,out i)).WithMessage("Phone number must be digits");
@@ -21,8 +21,8 @@ namespace ESMS.BackendAPI.ViewModels.Employees
             //RuleFor(x => x.Password).NotEmpty().WithMessage("Password can not be empty")
             //    .MinimumLength(6).WithMessage("Password needs to have at least 6 characters");
             //RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Confirm Password cannot be empty");
-            RuleFor(x => x.IdentityNumber).NotEmpty().WithMessage("Identity Number cannot be empty").Matches("^[0-9]{10,12}$").WithMessage("Identity number format must be from 10 to 12 digits");
-            RuleFor(x => x.Address).NotEmpty().WithMessage("Address cannot be empty").MaximumLength(100).WithMessage("Address can not exceed 100 characters");
+            RuleFor(x => x.IdentityNumber).NotEmpty().WithMessage("Identity Number can not be empty").Matches("^[0-9]{10,12}$").WithMessage("Identity number format must be from 10 to 12 digits");
+            RuleFor(x => x.Address).NotEmpty().WithMessage("Address can not be empty").MaximumLength(100).WithMessage("Address can not exceed 100 characters");
 
             //rulefor(x => x).custom((request, context) =>
             //{
--- ESMS.BackendAPI/ViewModels/Employees/EmpUpdateRequestValidator.cs
+++ ESMS.BackendAPI/ViewModels/Employees/EmpUpdateRequestValidator.cs
@@ -10,14 +10,18 @@ namespace ESMS.BackendAPI.ViewModels.Employees
     {
         public EmpUpdateRequestValidator()
         {
-            RuleFor(x => x.Name).NotEmpty().WithMessage("Name cannot be empty")
-                .MaximumLength(50).WithMessage("Name cannot exceed 50 characters").Matches(@"^(?:[^\W\d_]| )+$");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name can not be empty")
+                .MaximumLength(50).WithMessage("Name can not exceed 50 characters").Matches(@"^(?:[^\W\d_]| )+$").WithMessage("Name can not contain digits or special characters");
 
-            RuleFor(x => x.Email).NotEmpty().WithMessage("Email cannot be empty")
-                .Matches(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$").WithMessage("Email format is not correct");           
-            RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone number cannot be empty").Matches(@"(0[1-9])+([0-9]{8})\b").WithMessage("Phone number format is not correct");
-            RuleFor(x => x.IdentityNumber).NotEmpty().WithMessage("Identity Number cannot be empty").Matches("^[0-9]+$");
-            RuleFor(x => x.Address).NotEmpty().WithMessage("Address cannot be empty");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email can not be empty").MaximumLength(50).WithMessage("Email can not exceed 50 characters")
+                .Matches(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$").WithMessage("Email is not in the correct format");
+
+           
+            RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone number can not be empty").Matches(@"(0[1-9])+([0-9]{8})\b").WithMessage("Phone number must contain 10 digits starting off with 0");
+
+            RuleFor(x => x.IdentityNumber).NotEmpty().WithMessage("Identity Number can not be empty").Matches("^[0-9]{10,12}$").WithMessage("Identity number format must be from 10 to 12 digits");
+           
+            RuleFor(x => x.Address).NotEmpty().WithMessage("Address can not be empty").MaximumLength(100).WithMessage("Address can not exceed 100 characters");
         }
     }
 }
