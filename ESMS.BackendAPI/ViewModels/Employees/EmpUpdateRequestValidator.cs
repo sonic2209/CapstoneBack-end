7cad5e6cdcbf309557e6f1966ee1dc3b32e0dd6b
TomLesky <dinhbinh599@gmail.com>
2021-05-10 23:56:16 +0700

    Update employee validators

--- ESMS.BackendAPI/ViewModels/Employees/EmpUpdateRequestValidator.cs
+++ ESMS.BackendAPI/ViewModels/Employees/EmpUpdateRequestValidator.cs
@@ -10,18 +10,27 @@ namespace ESMS.BackendAPI.ViewModels.Employees
     {
         public EmpUpdateRequestValidator()
         {
-            RuleFor(x => x.Name).NotEmpty().WithMessage("Name can not be empty")
-                .MaximumLength(50).WithMessage("Name can not exceed 50 characters").Matches(@"^(?:[^\W\d_]| )+$").WithMessage("Name can not contain digits or special characters");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name can not be empty");            
+            RuleFor(x => x.Name).MaximumLength(50).WithMessage("Name can not exceed 50 characters")
+                .Matches(@"^(?:[^\W\d_]| )+$").WithMessage("Name can not contain digits or special characters")
+                .When(x => !String.IsNullOrWhiteSpace(x.Name));
 
             RuleFor(x => x.Email).NotEmpty().WithMessage("Email can not be empty").MaximumLength(50).WithMessage("Email can not exceed 50 characters")
                 .Matches(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$").WithMessage("Email is not in the correct format");
+            RuleFor(x => x.Email).MaximumLength(50).WithMessage("Email can not exceed 50 characters")
+                .Matches(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$").WithMessage("Email is not in the correct format").When(x => !String.IsNullOrWhiteSpace(x.Email));
 
-           
             RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone number can not be empty").Matches(@"(0[1-9])+([0-9]{8})\b").WithMessage("Phone number must contain 10 digits starting off with 0");
+            RuleFor(x => x.PhoneNumber).Matches(@"(0[1-9])+([0-9]{8})\b").WithMessage("Phone number must contain 10 digits starting off with 0")
+            .When(x => !String.IsNullOrWhiteSpace(x.PhoneNumber));
 
-            RuleFor(x => x.IdentityNumber).NotEmpty().WithMessage("Identity Number can not be empty").Matches("^[0-9]{10,12}$").WithMessage("Identity number format must be from 10 to 12 digits");
-           
-            RuleFor(x => x.Address).NotEmpty().WithMessage("Address can not be empty").MaximumLength(100).WithMessage("Address can not exceed 100 characters");
+            RuleFor(x => x.IdentityNumber).NotEmpty().WithMessage("Identity Number can not be empty");
+            RuleFor(x => x.IdentityNumber).Matches("^[0-9]{10,12}$").WithMessage("Identity number format must be from 10 to 12 digits")
+                .When(x => !String.IsNullOrWhiteSpace(x.IdentityNumber));
+
+            RuleFor(x => x.Address).NotEmpty().WithMessage("Address can not be empty");
+            RuleFor(x => x.Address).MaximumLength(100).WithMessage("Address can not exceed 100 characters")
+           .When(x => !String.IsNullOrWhiteSpace(x.Address));
         }
     }
 }
--- ESMS.BackendAPI/ViewModels/Employees/LoginRequestValidator.cs
+++ ESMS.BackendAPI/ViewModels/Employees/LoginRequestValidator.cs
@@ -9,11 +9,15 @@ namespace ESMS.BackendAPI.ViewModels.Employees
     {
         public LoginRequestValidator()
         {
-            RuleFor(x => x.Email).NotEmpty().WithMessage("Email cannot be empty")
-                .Matches(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email can not be empty");
+            RuleFor(x => x.Email)
+                .Matches(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$").WithMessage("Email is not in the correct format")
+                .When(x => !String.IsNullOrWhiteSpace(x.Email));
 
-            RuleFor(x => x.Password).NotEmpty().WithMessage("Password cannot be empty")
-                .MinimumLength(6).WithMessage("Password needs to have at least 6 characters");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password can not be empty");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password can not be empty")
+                .MinimumLength(6).WithMessage("Password needs to have at least 6 characters")
+                .When(x => !String.IsNullOrWhiteSpace(x.Email)); 
         }
     }
 }
