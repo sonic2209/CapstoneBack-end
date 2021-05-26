915c8cbb0764a0ef882ae4704b1a912a5ea92253
sonic2209 <51818304+sonic2209@users.noreply.github.com>
2021-05-26 22:51:55 +0700

    add getSkills feature

---
+++ ESMS.BackendAPI/ViewModels/Skill/ListSoftSkillVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESMS.BackendAPI.ViewModels.Skill
+{
+    public class ListSoftSkillVM
+    {
+        public List<ListSkillViewModel> MinumumSkill { get; set; }
+        public List<ListSkillViewModel> OptionalSkill { get; set; }
+    }
+}
